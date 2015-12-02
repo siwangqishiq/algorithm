96dee7b65ff571d34b694fb1895b3b4c357cc20a
unknown <525647740@qq.com>
2015-12-02 12:55:58 +0800

    插入排序算法

---
+++ 2_算法基础/insert_sort.c
@@ -0,0 +1,46 @@
+// ≤Â»Î≈≈–Ú 
+// ∞¥…˝–Ú≈≈–Ú
+
+#include <stdio.h>
+
+#define  SUCCESS    1
+#define  ERROR     -1
+
+//print array data
+void showArray(int *data,int len)
+{
+	int i = 0;
+	for(;i < len; i++)
+	{
+		printf("%d    ",data[i]);
+	}//end for i
+	printf("\n");
+}
+
+//insert sort
+//≤Â»Î≈≈–Ú µœ÷
+int insertSort(int *data,int len)
+{
+	int i = 1;
+	for(i = 1; i < len; i++)
+	{
+		int key = data[i];
+		int p = i-1;
+		while(p >= 0 && data[p] > key)
+		{
+			data[p+1] = data[p];
+			p--;
+		}//end for p
+		data[p+1] = key;
+	}//end for i
+}
+
+int main()
+{
+	int data[] = {9,3,1,0,4,10,3,2,8,7};
+	int len = sizeof(data)/sizeof(data[0]);
+	showArray(data, len);
+	insertSort(data, len);
+	showArray(data, len);
+	return SUCCESS;
+}
