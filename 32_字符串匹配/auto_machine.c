c6c0bc9b58f3d1f789395d7934def452bbbd6314
siwangqishiq <525647740@qq.com>
2017-07-08 19:24:01 +0800

    基于有限状态自动机的字符串匹配算法

--- 32_字符串匹配/auto_machine.c
+++ 32_字符串匹配/auto_machine.c
@@ -1,15 +1,15 @@
 //基于有限状态自动机的字符串匹配算法
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
-const char CHAR_SET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
-							'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-							'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-						
-
-const int CHAR_SET_SIZE = sizeof(CHAR_SET) / sizeof(char);
-
+#define TRUE     1
+#define FALSE   0
 #define MAX_SIZE 1024
+
+//const char CHAR_SET[] = {'a', 'b', 'c'};
+const char CHAR_SET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+const int CHAR_SET_SIZE = sizeof(CHAR_SET) / sizeof(char);
 
 struct _AutoMachine
 {
@@ -19,9 +19,7 @@ struct _AutoMachine
 	char accept;//可接受状态，当前自动机只存在一种可接受状态
 	char transFuc[MAX_SIZE][MAX_SIZE];//二维矩阵存贮的状态转移函数
 };
-
 typedef struct _AutoMachine AutoMachine;
-
 
 int createAutoMachine(char *pattern,int size,AutoMachine *mac);
 
@@ -31,6 +29,25 @@ int autoMachineReadChar(AutoMachine *mac,char c);//自动机读入字符  返回状态
 
 int charToIndex(char c);
 
+int isSuffix(char *str,int strSize,char *suffix,int suffixSize);//judge isSuffix 
+
+void debugMac(AutoMachine *mac);
+void showAutoMacTransFunc(AutoMachine *mac);
+
+int main(int argc,char *argv[]){
+	char str[]="abcdefghijklmmnopqstuvwxyz122223333333";
+	int str_size = sizeof(str) / sizeof(char) - 1;
+	char sub[] = "bc";
+	int sub_size = sizeof(sub) / sizeof(char)  - 1;
+	
+	printf("T = %s \nP = %s \n %d , %d \n", str, sub, str_size, sub_size);
+	
+	int p = autoMachineIndexOf(str,str_size,sub,sub_size);
+	printf("index = %d \n",p);
+	
+	return 0;
+}
+
 void debugMac(AutoMachine *mac){
 	printf("auto machine status list:\n");
 
@@ -38,25 +55,37 @@ void debugMac(AutoMachine *mac){
 		printf("status_%d\n",mac->status[i]);
 	}
 	printf("accept status_%d\n",mac->accept);
+
+	printf("test suffix\n");
+	char str[]="nihao  shijiehahaha";
+	int strSize = strlen(str) - 1;
+	char sub[]="ababaca";
+	int subSize = strlen(sub) - 1;
+
+	int ret = isSuffix(str , strSize , sub , subSize);
+	printf("suffix test result = %d \n", ret);
 	
-	for(int i = 0;i<CHAR_SET_SIZE;i++){
-		char ch = CHAR_SET[i];
-		printf("%c index = %d \n",ch ,charToIndex(ch));
-	}
+	//showAutoMacTransFunc(mac);
 }
 
-int main(int argc,char *argv[]){
-	char str[]="abcbcaaaaooopqqwwww111222333";
-	int str_size = sizeof(str) / sizeof(char);
-	char sub[] = "w111222";
-	int sub_size = sizeof(sub) / sizeof(char) - 1;
+int isSuffix(char *str,int strSize,char *suffix,int suffixSize){
+	if(suffixSize > strSize)
+		return FALSE;
 	
-	printf("%d , %d \n",str_size,sub_size);
+	if(suffixSize == 0)//子串长度为0 空串时   必然为str的后缀
+		return TRUE;
 	
-	int p = autoMachineIndexOf(str,str_size,sub,sub_size);
-	printf("index = %d \n",p);
+	// suffixSize <= strSize
+	int index = suffixSize - 1;
+	int strIndex = strSize -1;
 	
-	return 0;
+	while(index >= 0 ){
+		if(suffix[index--] != str[strIndex--]){
+			return FALSE;
+		}//end if
+	}//end while
+
+	return TRUE;
 }
 
 //根据匹配串 构建自动机
@@ -72,10 +101,59 @@ int createAutoMachine(char *pattern,int size,AutoMachine *mac){
 	//构造状态转移函数
 	memset(mac->transFuc,0,MAX_SIZE * MAX_SIZE);
 	
+	char tempStr[MAX_SIZE];
+	printf("Pattern ->%s\n",pattern);
+	
+	for(char status = mac->cur ; status <= mac->accept ; status++){ //遍历所有状态
+		for(int i = 0; i < CHAR_SET_SIZE; i++){//遍历所有字符
+			char ch = CHAR_SET[i];
+			memset(tempStr,0,MAX_SIZE);
+			//生成搜索串
+			strncpy(tempStr,pattern,status);
+			int len = strlen(tempStr);
+			tempStr[len] = ch;
+			tempStr[len + 1] = '\0';
+			int tempStrSize = status + 1;
+			//printf("tempStr ->%s , size = %d\n",tempStr,tempStrSize);
+			int maxPValue = mac->statusSize + 1;
+			
+			while(!isSuffix(tempStr,tempStrSize, pattern, maxPValue)){
+					maxPValue--;
+			}//end while
+			
+			//debug
+			char buf[1024];
+			strncpy(buf, pattern, maxPValue);
+			printf("T = %s     , subPattern = %s \n",tempStr, buf);
+			//debugend
+			
+			mac->transFuc[status][i] = maxPValue; 
+		}//end for i
+	}//end for status
+	
+	showAutoMacTransFunc(mac);
+	
 	return -1;
 }
 
+void showAutoMacTransFunc(AutoMachine *mac){//状态转移函数的显示sss
+	printf("status \t");
+	for(int i = 0;i < CHAR_SET_SIZE; i ++){
+		printf("%c \t",CHAR_SET[i]);
+	}//end for i
+	printf("\n");
+	
+	for(char i = 0; i < mac->statusSize;i++){
+		printf("%d",i);
+		for(int j = 0;j < CHAR_SET_SIZE; j++){
+			printf("\t%d ",mac->transFuc[i][j]);
+		}//end for i
+		printf("\n");
+	}//end for i
+}
+
 int autoMachineReadChar(AutoMachine *mac,char c){
+	//printf("read char %c\n",c);
 	mac->cur = mac->transFuc[mac->cur][charToIndex(c)];
 	return mac->cur;
 }
@@ -91,13 +169,18 @@ int autoMachineIndexOf(char *t,int len,char *p,int pLen) {
 	
 	AutoMachine am;
 	createAutoMachine(p,pLen,&am);
-	debugMac(&am);
+	//debugMac(&am);
 	
 	for(int i = 0;i < len ;i++){
 		if(autoMachineReadChar(&am,t[i]) == am.accept){
-			return i - pLen;
+			printf("pattern match!\n");
+			return i - pLen + 1;
 		}//end if
 	}//end for i
+	
+	if(am.cur != am.accept){
+		printf("pattern Not match!\n");
+	}
 	
 	return -1;
 }
