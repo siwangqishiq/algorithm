165f983ea62abdcdc20d74af63340291d44ae09f
siwangqishiq <panyi@yixin.im>
2020-12-22 15:04:32 +0800

    int to ro man

---
+++ leetcode/int_to_roman.cpp
@@ -0,0 +1,106 @@
+/**
+ * 
+ * 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+
+字符          数值
+I             1
+IV            4
+V             5
+IX            9
+X             10
+XL            40
+L             50
+XC              90
+C             100
+CD            400   
+D             500
+CM            900
+M             1000
+例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。
+
+示例 1:
+
+输入: 3
+输出: "III"
+
+示例 2:
+输入: 4
+输出: "IV"
+
+示例 3:
+输入: 9
+输出: "IX"
+
+示例 4:
+输入: 58
+输出: "LVIII"
+解释: L = 50, V = 5, III = 3.
+示例 5:
+
+输入: 1994
+输出: "MCMXCIV"
+解释: M = 1000, CM = 900, XC = 90, IV = 4
+ * 
+ * 
+ * */
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+string intToRoman(int num) {
+    if(num <= 0){
+        return string("");
+    }
+
+    map<int , string> bank;
+
+    bank[1] = string("I");
+    bank[4] = string("IV");
+    bank[5] = string("V");
+    bank[9] = string("IX");
+    bank[10] = string("X");
+    bank[40] = string("XL");
+    bank[50] = string("L");
+    bank[90] = string("XC");
+    bank[100] = string("C");
+    bank[400] = string("CD");
+    bank[500] = string("D");
+    bank[900] = string("CM");
+    bank[1000] = string("M");
+    
+   
+
+    string roman = string("");
+    int value = num;
+    while(value > 0){
+        for(map<int , string>::reverse_iterator iter = bank.rbegin() ; iter != bank.rend();iter++){
+            //cout << iter->first << " = " << iter->second << endl;
+            int key = iter->first;
+            if(value >= key){
+                value -= key;
+                roman += (iter->second);
+                break;
+            }
+        }
+    }//end while
+    return roman;
+}
+
+int main(){
+    cout << intToRoman(3) << endl;
+    cout << intToRoman(4) << endl;
+    cout << intToRoman(9) << endl;
+    cout << intToRoman(58) << endl;
+    cout << intToRoman(1994) << endl;
+    return 0;
+}
